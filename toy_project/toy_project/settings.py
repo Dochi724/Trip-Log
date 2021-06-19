147167f301dcfbdb1d87af4ae2ef57a7fb74b12e
Donghae <ldonghae320@gmail.com>
2021-06-19 17:17:28 +0900

    Design: index.html

--- toy_project/toy_project/settings.py
+++ toy_project/toy_project/settings.py
@@ -18,10 +18,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 #to get SECRET_KEY
 secret_file = os.path.join(BASE_DIR, 'secret.json')
 
